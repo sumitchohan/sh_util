36a8396494e1837a6cc5bc3640a526b980cf1029
Sumit Chohan <Sumit.Chohan@ttx.com>
2023-11-26 20:47:42 -0600

    wip

--- getProjects.sh
+++ getProjects.sh
@@ -2,4 +2,7 @@ organization="TTX"
 pat="${ado_pat}"
 url="https://dev.azure.com/$organization/_apis/projects?api-version=6.0"
 projectsJson=$(curl -u ":$pat" -H "Content-Type: application/json" -X GET $url)
-echo $projectsJson | jq '.value | .[] | "\(.id),\(.name)" ' 
+echo $projectsJson | jq '.value | .[] | "\(.id) \(.name)" ' | tr -d '"' | while read -r id name ; do
+    echo "id:$id;name:$name"
+    # your code goes here
+done
