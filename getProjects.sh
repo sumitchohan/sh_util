505e5591d923f329bad7b8b5abb475211d3d1835
Sumit Chohan <Sumit.Chohan@ttx.com>
2023-11-27 13:24:23 -0600

    wip

--- getProjects.sh
+++ getProjects.sh
@@ -2,7 +2,12 @@ organization="TTX"
 pat="${ado_pat}"
 url="https://dev.azure.com/$organization/_apis/projects?api-version=6.0"
 projectsJson=$(curl -u ":$pat" -H "Content-Type: application/json" -X GET $url)
-echo $projectsJson | jq '.value | .[] | "\(.id) \(.name)" ' | tr -d '"' | while read -r id name ; do
-    echo "id:$id;name:$name"
-    # your code goes here
-done
+echo "Project Id, Project Name, Repo Id, Repo Name, Repo Url" >  repositories.csv
+echo $projectsJson | jq '.value | .[] | "\(.id) \(.name)"' | tr -d '"\r' |  while read -r projectId projectName ; do
+    echo "getting details for projectId = $projectId and name=$projectName"   
+    url="https://dev.azure.com/$organization/$projectId/_apis/git/repositories?api-version=6.0" 
+    repositories=$(curl -u ":$pat" -H "Content-Type: application/json" -X GET $url) 
+    echo $repositories | jq '.value | .[] | "\(.id) \(.name) \(.url)" ' | tr -d '"'  | while read -r repoId repoName repoUrl ; do
+        echo "$projectId,$projectName,$repoId,$repoName,$repoUrl" | tr -d '\n' >>  repositories.csv
+    done 
+done 
